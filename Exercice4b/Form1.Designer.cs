89f13b369d1f59ab120a7f62cd241eb5def8464d
Jaume Plomer Suau <jaumeplomer@paucasesnovescifp.cat>
2020-10-14 19:54:47 +0200

    Commit #7 with exercice 4b
    I copy-pasted the exercice 4a and changed the sum and substact buttons for a listbox.
    It does the same as exercice 4a

---
+++ Exercice4b/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Exercice4b
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.operatorTextBox = new System.Windows.Forms.TextBox();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(166, 299);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(90, 38);
+            this.calculateButton.TabIndex = 0;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(565, 299);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(92, 38);
+            this.resetButton.TabIndex = 1;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(328, 243);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Selected opreation:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(305, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Select the operation to do:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(73, 82);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(73, 172);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(140, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // operatorTextBox
+            // 
+            this.operatorTextBox.Location = new System.Drawing.Point(356, 289);
+            this.operatorTextBox.Name = "operatorTextBox";
+            this.operatorTextBox.Size = new System.Drawing.Size(62, 22);
+            this.operatorTextBox.TabIndex = 6;
+            this.operatorTextBox.TextChanged += new System.EventHandler(this.operatorTextBox_TextChanged);
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(543, 111);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(132, 22);
+            this.resultTextBox.TabIndex = 7;
+            this.resultTextBox.TextChanged += new System.EventHandler(this.resultTextBox_TextChanged);
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 16;
+            this.listBox.Items.AddRange(new object[] {
+            "+",
+            "-"});
+            this.listBox.Location = new System.Drawing.Point(351, 79);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(81, 20);
+            this.listBox.TabIndex = 8;
+            this.listBox.SelectedIndexChanged += new System.EventHandler(this.listBox_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.operatorTextBox);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.calculateButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox operatorTextBox;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.ListBox listBox;
+    }
+}
+
---
+++ Exercice4b/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercice4b
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+            string operador = listBox.SelectedItem.ToString();
+            if (operador == "+")
+            {
+                int c = a + b;
+                resultTextBox.Text = c.ToString();
+            }
+            else if (operador == "-")
+            {
+                int c = a - b;
+                resultTextBox.Text = c.ToString();
+            }
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            resultTextBox.Clear();
+            operatorTextBox.Clear();
+        }
+
+        private void listBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string operador = listBox.SelectedItem.ToString();
+            if (operador == "+")
+            {
+                operatorTextBox.Text = "+";
+            }
+            else if (operador == "-")
+            {
+                operatorTextBox.Text = "-";
+            }
+        }
+
+        private void operatorTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void resultTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
