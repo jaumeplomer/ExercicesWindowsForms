7d0faa15fda3a69060a7c1091ba3c797dafd779e
Jaume Plomer Suau <jaumeplomer@paucasesnovescifp.cat>
2020-10-14 20:01:51 +0200

    Commit #8 with the exercise 7.
    Created a combobox with 5 colours and a textbox that shows the selected colour.

---
+++ Exercise7/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "The choosen colour is: " + comboBox1.SelectedItem.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
