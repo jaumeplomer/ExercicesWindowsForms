dc2f189bde800e56ce74f8608f7efbd0aee02b12
Jaume Plomer Suau <jaumeplomer@paucasesnovescifp.cat>
2020-10-14 20:52:28 +0200

    Commit #11 with exercise 10.
    Created a scrollbar and a textbox, and it is shown in the texbox the value of the scrollbar.

---
+++ Exercise10/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void scrollBar_Scroll(object sender, ScrollEventArgs e)
+        {
+            textBox1.Text = scrollBar.Value.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
